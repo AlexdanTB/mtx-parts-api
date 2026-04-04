576cb5ee69937a9eba14ab163ac166ce812e3b37
AlexdanTB <alexdanieltb220@gmail.com>
2026-04-04 00:18:09 -0500

    feat: creacion repository Usuario

---
+++ src/main/java/com/mtxparts/api/repository/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package com.mtxparts.api.repository;
+
+import com.mtxparts.api.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByEmail(String email);
+}
