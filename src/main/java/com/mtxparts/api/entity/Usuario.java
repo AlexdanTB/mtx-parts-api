ff634b5e505963934b6453811dadf6173951a7b5
AlexdanTB <alexdanieltb220@gmail.com>
2026-04-04 00:12:37 -0500

    feat: anotaciones entity Usuario

--- src/main/java/com/mtxparts/api/entity/Usuario.java
+++ src/main/java/com/mtxparts/api/entity/Usuario.java
@@ -1,8 +1,8 @@
 package com.mtxparts.api.entity;
 
 import com.mtxparts.api.role.Rol;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -11,10 +11,20 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Size(min = 10, max = 45)
     private String nombre_completo;
+
+    @Column(unique = true)
     private String email;
+
     private String password;
+
+    @Enumerated(EnumType.STRING)
     private Rol rol;
 
 }
