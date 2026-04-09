bbe7dac966aa5bc5febea0a12dfab2e8b82afc89
AlexdanTB <alexdanieltb220@gmail.com>
2026-04-09 16:28:45 -0500

    feat: creacion entity Producto

---
+++ src/main/java/com/mtxparts/api/entity/Producto.java
@@ -0,0 +1,12 @@
+package com.mtxparts.api.entity;
+
+import java.math.BigDecimal;
+
+public class Producto {
+    private Long id;
+    private String sku;
+    private String nombre;
+    private String descripcion;
+    private BigDecimal precio;
+    private int stock;
+}
